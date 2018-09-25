f61dd353e7d36e3f7efeb4ff84cf091adaa4c9ca
thenerdjake <jbraun6@wisc.edu>
2018-09-24 21:38:42 -0500

    its buggy but this way you can see it

--- parser.c
+++ parser.c
@@ -6,6 +6,30 @@
 
 
 #include <stdio.h>
+#include <dirent.h>
+
+char* state_info;
+struct dirent *myFile;
+char* random;
+
+char* stat_U(char* PID){
+	
+	char* path;
+	char* str1;
+	char* str2;
+	char* str3;
+	char* str4;
+	
+	sprintf(path, "/Proc/%s", PID);
+	FILE* fp = fopen(path, "r");
+	fscanf(fp, "%s %s %s %s", str1, str2, str3, str4);
+	
+	
+	return state_info;
+}
+
+
+
 
 int main(int argc, char **argv)
 {
