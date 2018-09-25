360f09e07df0ab6a4ae5da3e50a396f77d4e7147
Dan Xiao <dxiao@rockhopper-08.cs.wisc.edu>
2018-09-25 16:24:35 -0500

    Not finished

--- list.c
+++ list.c
@@ -3,13 +3,51 @@
  * 
  * 
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <ctype.h>
+#include <unistd.h>
 
-
-import std.stdio;
-
-int main(string[] args)
+//tempory main
+int main()
 {
+	DIR *dir = opendir("/proc");
+	struct dirent* dirp;
+	long pid;
+
+	if(dir == NULL){
+		printf("No directory found.\n");
+		exit(0);
+	}
+
+
 	
+	// Get user id
+	int id = getuid();
+	printf("\n%d\n", id);
+	
+	// Iterate through /proc
+	
+	int count = 0;
+	while((dirp = readdir(dir)) != NULL){
+		if(!isdigit(*dirp->d_name))
+			continue;
+		
+
+		pid = strtol(dirp->d_name, NULL, 10);
+		printf("%li ", pid);
+		if(count == 20){
+			printf("\n");
+			count = 0;
+		}
+		count++;
+	}
+	
+
+
+	closedir(dir);
 	return 0;
 }
-
