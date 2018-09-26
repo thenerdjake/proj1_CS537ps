9c734aa36d2e7b049894e1eabef303925abc5ba5
thenerdjake <jbraun6@wisc.edu>
2018-09-26 02:57:44 -0500

    it compiles, u is really hard for some reason, not done but sleep

--- parser.c
+++ parser.c
@@ -7,16 +7,22 @@
 
 #include <stdio.h>
 #include <dirent.h>
+#include <unistd.h>
 
 char* state_info;
 struct dirent *myFile;
 char* random;
 const int path_size = 80;
+int sec;
+int min;
+int hour;
+int clock;
+char* time[80];
 
 char* stat_U(char* PID){
 	
 	char* path[80];
-	char* str[80];
+	char* str[1000];
 	
 	sprintf(path, "/proc/%s/stat", PID);
 	printf("%s\n", path);
@@ -29,10 +35,28 @@ char* stat_U(char* PID){
 		fscanf(fp, "%s", str);
 	}
 	printf("%s\n", str);
-	
-	
-	
-	return state_info;
+	clock = atoi(str);
+	printf("%d\n", clock);
+	printf("%d\n", sysconf(_SC_CLK_TCK));
+	sec = clock / sysconf(_SC_CLK_TCK);
+	printf("%d\n", sec);
+	while(sec > 60){
+		sec = sec-60;
+		min++;
+	}
+	/*while(min > 60){
+		min = min-60;
+		hour++;
+	}
+	* */
+	if ( min < 10){
+		sprintf(time, "%d:0%d", hour, min);
+	}
+	else{
+		sprintf(time, "%d:%d", hour, min);
+	}
+	printf("%s\n",time);
+	return time;
 }
 
 
@@ -40,7 +64,7 @@ char* stat_U(char* PID){
 
 int main(int argc, char **argv)
 {
-	char* PID= "515";
+	char* PID= "519";
 	printf("%s\n", PID);
 	stat_U(PID);
 	return 0;
