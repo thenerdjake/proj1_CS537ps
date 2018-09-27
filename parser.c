83ed7df689945aaabdfa5e68cafad67f9f0b0441
thenerdjake <jbraun6@wisc.edu>
2018-09-27 02:56:27 -0500

    cleaned parser up a bit, should be ready to go but probs gonna take the printing out of parser and into main

--- parser.c
+++ parser.c
@@ -24,65 +24,61 @@ char state[1];
 int size;
 char cmd[1000];
 
+//the U option
 int stat_U(int PID){
-	
-	
+	//creating the path to stat with the given PID
 	sprintf(path, "/proc/%d/stat", PID);
-	//printf("%s\n", path);
 	FILE* fp = fopen(path, "r");
-	if (fp == NULL){
-		//printf("error\n");
-	}
+	//take the 14th string
 	fscanf(fp, "%*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %s", str);
-	//printf("%s\n", str);
+	//change the string to an int for export
 	clock = atoi(str);
-	//printf("%d\n", sysconf(_SC_CLK_TCK));
+	//does not say to divide by clock so I commented out that
 	//sec = clock / sysconf(_SC_CLK_TCK);
-	//printf("%d\n", sec);
 	return clock;
 }
 
 char* stat_s(int PID){
+	//creating path to stat with given PID
 	sprintf(path, "/proc/%d/stat", PID);
 	FILE* fp = fopen(path, "r");
+	//take the third string
 	fscanf(fp, "%*s %*s %s", state);
-	printf("%s\n", state);
 	return state;
 }
 
 int stat_S(int PID){
+	//creating path to stat with given PID
 	sprintf(path, "/proc/%d/stat", PID);
 	FILE* fp = fopen(path, "r");
-	if (fp == NULL){
-		//printf("error\n");
-	}
+	//take the 15th string
 	fscanf(fp, "%*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %s", str);
+	//change the string to int to output
 	clock = atoi(str);
-	printf("%d\n", clock);
 	return clock;
 }
 
 int stat_v(int PID){
+	//creating path to statm with given PID
 	sprintf(path, "/proc/%d/statm", PID);
 	FILE* fp = fopen(path, "r");
-	if (fp == NULL){
-		//printf("error\n");
-	}
+	//take the first string in statm
 	fscanf(fp, "%s", str);
+	//convert it to int
 	size = atoi(str);
 	return clock;
 }
 
 char* stat_c(int PID){
-	sprintf(path, "/proc/%d/cmdline", PID);
-	FILE* fp = fopen(path, "r");
-	if (fp == NULL){
-		//printf("error\n");
-	}
 	size_t length;
 	char* buff;
+	//creating path to cmdline with given PID
+	sprintf(path, "/proc/%d/cmdline", PID);
+	FILE* fp = fopen(path, "r");
+	//read in to buffer from the file and output length
 	length = fread(cmd, sizeof(char), 100, fp);
 	buff = cmd;
+	//copy that buffer
 	while (buff < cmd + length){
 		printf("%s ", buff);
 		buff += strlen (buff) + 1;
@@ -90,10 +86,10 @@ char* stat_c(int PID){
 	return cmd;
 }
 
+//testing main
 int main(int argc, char **argv)
 {
 	int PID= 515;
-	//printf("%d\n", PID);
 	stat_c(PID);
 	return 0;
 }
