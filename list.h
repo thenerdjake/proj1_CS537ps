71f5a46aa7218467201dfb11847eb7eb252edf66
Dan Xiao <dxiao@royal-01.cs.wisc.edu>
2018-09-26 20:53:02 -0500

    header file for list.c

---
+++ list.h
@@ -0,0 +1,18 @@
+#ifndef __LIST_H__
+#define __LIST_H__
+
+#define BUFSIZE 1000
+
+/* Function Prototypes */
+/* Defined in list.c */
+
+// Returns an int pointer
+int* List();
+
+// Take take in int paramter to be added in as filepath 
+void Search(int processNum);
+
+// Returns size of an array
+int Size();
+
+#endif
