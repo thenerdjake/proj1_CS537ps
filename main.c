54ecf262c2296f3b7b2e1db4662fe52a2b5bd6db
Dan Xiao <dxiao@snares-04.cs.wisc.edu>
2018-09-27 00:51:46 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "list.h"
+#include "processing.h"
+
+int main(int c, char **argv){
+	arguments(c, argv);	
+
+	return 0;
+}
---
+++ processing.h
@@ -0,0 +1,7 @@
+#ifndef __PROCESSING_H__
+#define __PROCESSING_H__
+
+void arguments(int argc, char **argv);
+
+
+#endif
