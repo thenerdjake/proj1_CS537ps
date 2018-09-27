71f45a7869dd25c411cb9b3bf3f3b58d56aa6e62
Dan Xiao <dxiao@snares-04.cs.wisc.edu>
2018-09-27 04:32:39 -0500

    Was able to test functions from parser and list successfully

--- main.c
+++ main.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 #include "list.h"
-#include "processing.h"
+#include "parser.h"
 
 int main(int c, char **argv){
-	arguments(c, argv);	
+	int *p = List("/proc");
+	
+	// Test functions
+	// print user processes
+	for(int i = 0; i < Size(); i++){
+		printf("PID: %-12d ", *(p + i));
+		printf("utime: %-12d ", stat_U(*(p+i)));
+		printf("State: %-12s ", stat_s(*(p+i)));
+		printf("stime: %-12d ", stat_S(*(p+i)));
+		printf("Size: %-12d ", stat_v(*(p+i)));
+		printf("cmd: ");
+		stat_c(*(p+i));
+		printf("\n");
+
+	}
+	printf("\n");	
+
+	
+	
 
 	return 0;
 }
