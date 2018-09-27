e6d9da9e1b30931a5e0efe4a2c62398cdef423b9
thenerdjake <jbraun6@wisc.edu>
2018-09-27 11:30:31 -0500

    just try

--- main.c
+++ main.c
@@ -15,28 +15,28 @@ int main(int c, char **argv){
 		if(get_provided_pid()){
 			int PID = get_pid();
 			if (PID <= 0){
-				printf("That PID doesn't exist\n");
+				printf("That isn't a valid PID\n");
 				return 1;
 			}
 			if (!exist_PID(PID)){
-				printf("please provide a real PID, thank you\n");
+				printf("That PID does not exist\n");
 				return 1;
 			}
-			printf("PID: %d ", PID);
+			printf("PID:%d ", PID);
 			if (!get_not_contain_U()){
-				printf("utime: %d ", stat_U(PID));
+				printf("utime=%d  ", stat_U(PID));
 			}
 			if (get_contain_s()){
-				printf("State: %s ", stat_s(PID));
+				printf("State=%s  ", stat_s(PID));
 			}
 			if (get_contain_S()){
-				printf("stime: %d ", stat_S(PID));
+				printf("stime=%d  ", stat_S(PID));
 			}
 			if (get_contain_v()){
-				printf("Size: %d ", stat_v(PID));
+				printf("Size=%d  ", stat_v(PID));
 			}
 			if (!get_not_contain_c()){
-				printf("cmd: ");
+				printf("cmd=");
 				stat_c(PID);
 			}
 			printf("\n");
@@ -47,12 +47,29 @@ int main(int c, char **argv){
 		}
 	}
 	
-	//int *p = List("/proc");
-	if (get_empty()){
-		printf("test");
-	}
-	if (get_contain_p()){
-		
+	int *p = List("/proc");
+	if (!get_contain_p()){
+		for(int i = 0; i < Size(); i++){
+			printf("PID=%d  ", *(p + i));
+			if (!get_not_contain_U()){
+				printf("utime=%d  ", stat_U(*(p+i)));
+			}
+			if (get_contain_s()){
+				printf("State=%s  ", stat_s(*(p+i)));
+			}
+			if (get_contain_S()){
+				printf("stime=%d  ", stat_S(*(p+i)));
+			}
+			if (get_contain_v()){
+				printf("Size=%d  ", stat_v(*(p+i)));
+			}
+			if (!get_not_contain_c()){
+				printf("cmd=");
+				stat_c(*(p+i));
+			}
+			printf("\n");
+		}
+		printf("\n");
 	}
 
 	
